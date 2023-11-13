d8365fe1aa66cfff6ef10e91bc830318ae77fb7b
Iamthevk <vijay.vuex@gmail.com>
2023-11-13 17:05:03 +0530

    input validation and SelectRating component

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,14 +1,32 @@
 import Button from "../shared/Button";
 import Card from "../shared/Card";
 import { useState } from "react";
+import SelectRating from "./SelectRating";
 const FeedbackForm = () => {
   const [text, setText] = useState("");
+  const [rating, setRating] = useState(5);
+  const [message, setMessage] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(false);
 
-  const handleUserInput = (e) => setText(e.target.value);
+  const handleUserInput = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 chracters");
+      setBtnDisabled(true);
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+    setText(e.target.value);
+  };
+  console.log(rating);
   return (
     <Card>
       <form>
         <h2>How would you rate your service with us</h2>
+        <SelectRating select={(rating) => setRating(rating)} />
         <div>
           <input
             type="text"
@@ -16,8 +34,11 @@ const FeedbackForm = () => {
             value={text}
             onChange={handleUserInput}
           />
-          <Button type="submit">Send</Button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
