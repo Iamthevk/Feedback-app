6e8ee8227f6d5dfe2705d9bd7b0657ebaebd4bc8
Iamthevk <vijay.vuex@gmail.com>
2023-11-13 19:02:53 +0530

    feedback item fade animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,19 +1,25 @@
 import FeedbackItem from "./FeedbackItem";
+import { motion, AnimatePresence } from "framer-motion";
 const FeedbackList = ({ feedback, handleDeleteItem }) => {
   return (
     <div className="feedback-list">
       {feedback.length === 0 ? (
         <p style={{ fontSize: "30px" }}>No Feedback Yet</p>
       ) : (
-        feedback?.map((item) => {
-          return (
-            <FeedbackItem
-              key={item.id}
-              item={item}
-              handleDeleteItem={handleDeleteItem}
-            />
-          );
-        })
+        <AnimatePresence>
+          {feedback?.map((item) => {
+            return (
+              <motion.div
+                key={item.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+              >
+                <FeedbackItem item={item} handleDeleteItem={handleDeleteItem} />
+              </motion.div>
+            );
+          })}
+        </AnimatePresence>
       )}
     </div>
   );
--- src/data/FeedbackData.js
+++ src/data/FeedbackData.js
@@ -1,17 +1,17 @@
 const FeedbackData = [
   {
     id: 1,
-    rating: 10,
+    rating: 5,
     text: "Thank you for your exceptional work! Your dedication and creativity have greatly contributed to the project's success. Your attention to detail and innovative solutions have not gone unnoticed. We truly appreciate your hard work and commitment to excellence.",
   },
   {
     id: 2,
-    rating: 9,
+    rating: 4,
     text: "Your effort on the project is commendable; however, consider refining certain aspects for even greater impact. Providing more context in specific sections would enhance clarity.",
   },
   {
     id: 3,
-    rating: 9,
+    rating: 3,
     text: "Collaboration with you has been a pleasure. Your open communication and willingness to share ideas have fostered a positive working environment.",
   },
 ];
